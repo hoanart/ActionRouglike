4f6c126a0fc74aa0a4b8a7641131f8253ee48d91
Seongho <hoanart@naver.com>
2023-10-26 19:05:08 +0900

    Initial commit

---
+++ Source/ActionRouglike/ActionRouglikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRouglikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGLIKE_API AActionRouglikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
